72cdc73d80acfaf9de35a43dbb888034eafddb97
August <Augusthauerslev@outlook.com>
2023-04-23 21:30:51 +0200

    added columns per request

--- Server/routers/biografRouter.js
+++ Server/routers/biografRouter.js
@@ -4,7 +4,7 @@ import db from '../database/connection.js';
 const biografRouter = new Router();
 
 biografRouter.get("/api/biograf", (req, res) => {
-    db.query('SELECT * from theaterdb.biografer', (err, rows, fields) => {
+    db.query('SELECT * from theaterdb.biografer ORDER BY theaterdb.biografer.Postnr', (err, rows, fields) => {
         if (err) throw err
         res.send({ biografer: rows })
     })
