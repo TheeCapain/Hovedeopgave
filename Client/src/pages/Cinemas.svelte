0c4d65d4c2180f093a67ad76e52da4092fdf7f19
August <Augusthauerslev@outlook.com>
2023-05-11 12:59:42 +0200

    fixed som search issues

--- Client/src/pages/Cinemas.svelte
+++ Client/src/pages/Cinemas.svelte
@@ -140,30 +140,7 @@
                     bind:value={postNr}
                     required
                 />
-                <button
-                    id="dropdownBgHoverButton"
-                    data-dropdown-toggle="dropdownBgHover"
-                    class="text-white w-full bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                    type="button"
-                    >Vælg Postnr<svg
-                        class="w-4 h-4 ml-2"
-                        aria-hidden="true"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                        ><path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            d="M19 9l-7 7-7-7"
-                        /></svg
-                    ></button
-                >
-                <div
-                    id="dropdownBgHover"
-                    class="z-10 h-72 hidden overflow-auto bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-                />
+                
             </li>
             <li>
                 <h3 class="mb-4 font-semibold text-gray-900 dark:text-white">
