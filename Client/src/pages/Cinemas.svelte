693c367afe954e727afdcc86d4356437434f75b6
August <Augusthauerslev@outlook.com>
2023-12-24 11:41:49 +0100

    last touches

--- Client/src/pages/Cinemas.svelte
+++ Client/src/pages/Cinemas.svelte
@@ -45,7 +45,6 @@
         }
     }
 
-
     /* async function searchTheater() {
         const movie_search = {
             cinemaName: name,
@@ -77,7 +76,7 @@
             cinema_list = data;
             console.log(cinema_list);
             resultAmount = cinema_list.length;
-            sortName()
+            sortName();
             return data;
         } catch (error) {
             console.error("Error:", error);
@@ -163,41 +162,40 @@
     >
         <ul class="space-y-2 font-medium">
             <li>
-                <form>
-                    <label
-                        for="default-search"
-                        class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-                        >Search</label
+                <p class="text-1xl font-extrabold text-white">
+                    <a href="https://www.biografmuseet.dk/">Biograf Museet's Db</a>
+                </p>
+            </li>
+
+            <li>
+                <div class="relative">
+                    <div
+                        class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
                     >
-                    <div class="relative">
-                        <div
-                            class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+                        <svg
+                            aria-hidden="true"
+                            class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                            ><path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                            /></svg
                         >
-                            <svg
-                                aria-hidden="true"
-                                class="w-5 h-5 text-gray-500 dark:text-gray-400"
-                                fill="none"
-                                stroke="currentColor"
-                                viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg"
-                                ><path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                                /></svg
-                            >
-                        </div>
-                        <input
-                            type="search"
-                            id="default-search"
-                            class="dark:hover:bg-text-700 block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Bio navn"
-                            bind:value={name}
-                            required
-                        />
                     </div>
-                </form>
+                    <input
+                        type="search"
+                        id="default-search"
+                        class="dark:hover:bg-text-700 block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="Bio navn"
+                        bind:value={name}
+                        required
+                    />
+                </div>
             </li>
             {#if resultAmount != undefined}
                 <li>
@@ -208,16 +206,6 @@
                     >
                 </li>
             {/if}
-            <li>
-                <input
-                    type="search"
-                    id="default-search"
-                    class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Bio Navn"
-                    bind:value={name}
-                    required
-                />
-            </li>
 
             <li>
                 <span class="ml-3">
